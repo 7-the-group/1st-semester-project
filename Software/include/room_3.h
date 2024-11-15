b5db45eedf44d064f5098a6f4b13753bfd3159a5
Dubrauka <dubravajakub@gmail.com>
2024-11-15 13:57:50 +0100

    Code update for room 3

--- Software/include/room_3.h
+++ Software/include/room_3.h
@@ -2,8 +2,30 @@
 
 #define ROOM_3_H_INCLUDED
 
+#include "color.h"
 
+#define POTENTIOMETER1ROOM3 2
+#define POTENTIOMETER2ROOM3 7
+#define REDLED 3
+#define GREENLED 5
+#define BLUELED 6
+#define BUTTONROOM3 4
 
 void init_room3();
+void update_room3();
+
+int check_button_room_3();
+void turn_on_light_room_3();
+void turn_off_light_room_3();
+void switch_light_room_3();
+int get_status_of_light_room_3();
+
+struct ColorRGB get_color_of_light_RGB_room_3();
+struct ColorHSV get_color_of_light_HSV_room_3();
+void set_color_of_light_RGB_room_3(ColorRGB);
+void set_color_of_light_HSV_room_3(ColorHSV);
+
+int read_pot_1_value_room_3();
+int read_pot_2_value_room_3();
 
 #endif
--- Software/src/room_3.c
+++ Software/src/room_3.c
@@ -1,11 +1,26 @@
 #include <room_3.h>
 #include <avr/io.h>
+#include "adcpwm.h"
+
+unsigned char buttonStateRoom3 = 0;
+int rgbMenuActive = 0;
 
 void init_room_3()
 {
     // set all adc pins to input
+    DDRC &= ~(1 << POTENTIOMETER1ROOM3); // set potentiometer 1 to input
+    DDRD &= ~(1 << POTENTIOMETER2ROOM3); // set potentiometer 2 to input
+    DDRD &= ~(1 << BUTTONROOM3); // set button to input
+
+    DDRD |= (1 << REDLED) | (1 << GREENLED) | (1 << BLUELED); // set leds to output
+
+    PORTD &= ~((1 << REDLED) | (1 << GREENLED) | (1 << BLUELED)); // leds turned off initially
+    PORTD &= ~(1 << BUTTONROOM3); // set button to 0
+
     // initialize adc from adcpwm.h library
+    adc_init();
     // initialize pwm from adcpwm.h library
+    pwm3_init();
 }
 
 void update_room_3()
@@ -17,26 +32,30 @@ void update_room_3()
 int check_button_room_3()
 {
   // return 1 if button is pressed otherwise 0
+  return (PIND & (1 << BUTTONROOM3));
 }
 
 void turn_on_light_room_3()
 {
-    
+    PORTD |= (1 << REDLED) | (1 << GREENLED) | (1 << BLUELED);
 }
 
 void turn_off_light_room_3()
 {
-
+    PORTD |= ~((1 << REDLED) | (1 << GREENLED) | (1 << BLUELED));
 }
 
 void switch_light_room_3()
 {
     // switch state of a light
+    if (get_status_of_light_room_3()) turn_off_light_room_3();
+    else turn_on_light_room_3();
 }
 
 int get_status_of_light_room_3()
 {
     // return 1 if light is on otherwise 0
+    return PORTD & ((1 << REDLED) | (1 << GREENLED) | (1 << BLUELED));
 }
 
 struct ColorRGB get_color_of_light_RGB_room_3()
@@ -64,6 +83,7 @@ void set_color_of_light_HSV_room_3(struct ColorHSV hsv)
 int read_pot_1_value_room_3()
 {
     // return value of potentiometer 1
+    return PINC2;
 }
 
 int read_pot_2_value_room_3()
