cd4d95677679e82f2144c3b0b6a69a5bd23713fa
Milan Caban <80208573+AiwendilsCode@users.noreply.github.com>
2024-12-05 15:05:36 +0100

    Update display.h to receive Menu_element** instead of Menu_element*.

--- Software/include/display.h
+++ Software/include/display.h
@@ -2,7 +2,7 @@
 #define DISPLAY_H_INCLUDED
 #include "menu.h"
 void init_display();
-void update_display(Menu_element* elems, int elements_count, int element_hovered, int element_selected); 
+void update_display(Menu_element** elems, int elements_count, int element_hovered, int element_selected); 
 //if no elment is hovered element_hovered=-1
 //if no element is selected element_selected=-1
 //one of variables(hovered,selected) will always be -1
