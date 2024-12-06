7384452dc4332e8409ba34ba0318d8631e01a294
Milan Caban <80208573+AiwendilsCode@users.noreply.github.com>
2024-12-06 01:44:43 +0100

    Add not saved code for room 1 from previous commit.

--- Software/include/room_1.h
+++ Software/include/room_1.h
@@ -6,9 +6,11 @@ void update_room_1();
 
 void turn_on_light_room_1();
 void turn_off_light_room_1();
+void switch_light_room_1();
 void set_light_intensity_room_1(int intensity);
 int get_light_intensity_room_1();
 
+int check_btn_room_1();
 int read_pot_room_1();
 
 #endif
