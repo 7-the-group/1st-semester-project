963c3d3cc0ede7f5dfe3f0b3c3475bcc44658b76
Milan Caban <80208573+AiwendilsCode@users.noreply.github.com>
2024-12-14 19:10:44 +0100

    Add control_panel.c implementation for menu.

--- Software/src/control_panel.c
+++ Software/src/control_panel.c
@@ -3,21 +3,30 @@
 #include <display.h>
 #include <avr/io.h>
 #include <adcpwm.h>
+#include <stdio.h>
+#include <util/delay.h>
 
-#include <room_2.h>
-#include <room_3.h>
-#include <door.h>
+#include <functions_for_rooms.h>
 
+#define SDU_BOARD_TESTING 0
+
+#if !SDU_BOARD_TESTING
 #define BTN_UP      7 // port D
 #define BTN_DOWN    0 // port B
 #define BTN_SELECT  3 // port B
 #define BTN_BACK    4 // port B
-#define POT_PIN     0 // port C
+#else
+#define BTN_UP      0 // port D
+#define BTN_DOWN    1 // port D
+#define BTN_SELECT  2 // port D
+#define BTN_BACK    3 // port D
+#endif
 
 enum
 {
     IN_MENU,
     IN_FLOAT_ELM,
+    IN_BOOL_ELM,
     IN_ENUM_ELM,
 } current_position;
 
@@ -27,41 +36,61 @@ typedef enum
     PRESS_BTN_DOWN,
     PRESS_BTN_SELECT,
     PRESS_BTN_BACK,
-    CHANGE_POT
+    CHANGE_POT,
+    NOTHING
 } What_happened;
 
-Menu_element* curr_element = 0;
+void show_curr_elems_on_display();
 
 int check_btn_up()
 {
-    return PIND & (1 << BTN_UP) != 0;
+    if ((PIND & (1 << BTN_UP)) == 0)
+    {
+        _delay_ms(100);
+        return 1;
+    }
+
+    return 0;
 }
 
 int check_btn_down()
 {
-    return PINB & (1 << BTN_DOWN) != 0;
+    if ((PINB & (1 << BTN_DOWN)) == 0)
+    {
+        _delay_ms(100);
+        return 1;
+    }
+
+    return 0;
 }
 
 int check_btn_select()
 {
-    return PINB & (1 << BTN_SELECT) != 0;
+    if ((PINB & (1 << BTN_SELECT)) == 0)
+    {
+        _delay_ms(100);
+        return 1;
+    }
+
+    return 0;
 }
 
 int check_btn_back()
 {
-    return PINB & (1 << BTN_BACK) != 0;
-}
+    if ((PINB & (1 << BTN_BACK)) == 0)
+    {
+        _delay_ms(100);
+        return 1;
+    }
 
-uint16_t read_pot_value()
-{
-    return adc_read(POT_PIN);
+    return 0;
 }
 
 void init_control_panel()
 {
     init_display();
     init_menu();
-    adc_init();
+    //adc_init();
 
     // set input and pull up for button up
     DDRD &= ~(1 << BTN_UP);
@@ -78,6 +107,8 @@ void init_control_panel()
     // set input and pull up for button back
     DDRB &= ~(1 << BTN_BACK);
     PORTB |= (1 << BTN_BACK);
+
+    show_curr_elems_on_display();
 }
 
 void show_curr_elems_on_display()
@@ -88,6 +119,34 @@ void show_curr_elems_on_display()
 
     int hovered_elm = get_hovered_elm_idx();
     int selected_elm = get_selected_elm_idx();
+/*
+    for (int i = 0; i < elms_count; i++)
+    {
+        if (i == hovered_elm - 1)
+        {
+            printf("* ");
+        }
+        else if (i == selected_elm - 1)
+        {
+            printf("> ");
+        }
+        else
+        {
+            printf("  ");
+        }
+
+        if (elms[i]->type == MENU_FILE)
+        {
+            printf("%s ", elms[i]->element.file.Name);
+            printf("%3.2f", elms[i]->element.file.value);
+        }
+        else if (elms[i]->type == MENU_FOLDER)
+        {
+            printf("%s", elms[i]->element.folder.Name);
+        }
+
+        printf("\n");
+    }*/
 
     update_display(elms, elms_count, hovered_elm, selected_elm);
 }
@@ -147,10 +206,34 @@ What_happened check_controls()
     {
         return PRESS_BTN_UP;
     }
+
+    return NOTHING;
 }
 
 void update_control_panel()
-{
+{/*
+    char pressed_control;
+
+    printf("Enter control: ");
+    scanf("%c", &pressed_control);
+    printf("\n");
+
+    switch(pressed_control)
+    {
+        case 'w': // up
+            react_btn_up_pressed();
+            break;
+        case 's': // select
+            react_btn_select_pressed();
+            break;
+        case 'z': // down
+            react_btn_down_pressed();
+            break;
+        case 'q': // back
+            react_btn_back_pressed();
+            break;
+    }*/
+    
     switch(check_controls())
     {
         case PRESS_BTN_UP:
@@ -168,5 +251,53 @@ void update_control_panel()
         case CHANGE_POT:
             react_pot_value_changed();
             break;
+        case NOTHING:
+            break;
     }
+
+    Menu_element* curr_elems[4];
+    int element_count;
+
+    get_current_elms(curr_elems, &element_count);
+
+    for (int i = 0; i < element_count; i++)
+    {
+        if (curr_elems[i]->type == MENU_FILE)
+        {
+            get_value_func get_value = get_getter_func(curr_elems[i]->element.file.value);
+            float new_value = get_value();
+
+            if (new_value != curr_elems[i]->element.file.value)
+            {
+                curr_elems[i]->element.file.value = new_value;
+                update_file_element(curr_elems[i]);
+            }
+        }
+    }
+
+/*
+    switch (curr_element->type)
+    {
+        case MENU_FOLDER:
+            current_position = IN_MENU;
+            break;
+        case MENU_FILE:
+            if (curr_element->element.file.selected)
+            {
+                switch (curr_element->element.file.type)
+                {
+                    case FLOAT:
+                        current_position = IN_FLOAT_ELM;
+                        break;
+                    case BOOL:
+                        current_position = IN_BOOL_ELM;
+                        break;
+                }
+            }
+            else
+            {
+                current_position = IN_MENU;
+            }
+    }*/
+    
 }
