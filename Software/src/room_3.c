bd2b791f5591bd7c67de0274df9e0bf3bfbe6291
Dubrauka <dubravajakub@gmail.com>
2024-12-18 22:47:26 +0100

    Update room_3.c

--- Software/src/room_3.c
+++ Software/src/room_3.c
@@ -7,7 +7,7 @@
 unsigned char previousButtonStateRoom3 = 1;
 ColorRGB RGBcolor;
 int prev_potentiometer_1;
-int prev_potentiometer_2;
+// int prev_potentiometer_2;
 
 void init_room_3()
 {
@@ -17,9 +17,9 @@ void init_room_3()
     pwm3_init();
 
     pot_1_controlled_val_room_3 = H;
-    pot_2_controlled_val_room_3 = V;
+    // pot_2_controlled_val_room_3 = V;
     // set all adc pins to input
-    DDRC &= ~(1 << POTENTIOMETER1_ROOM3); // set potentiometer 1 to input
+    // DDRC &= ~(1 << POTENTIOMETER1_ROOM3); // set potentiometer 1 to input
     DDRD &= ~(1 << POTENTIOMETER2_ROOM3); // set potentiometer 2 to input
 
     DDRD &= ~(1 << BUTTON_ROOM3); // set button to input
@@ -35,7 +35,7 @@ void update_room_3()
 {
     // measure pots
     int potentiometer_1 = read_pot_1_value_room_3();
-    int potentiometer_2 = read_pot_2_value_room_3();
+    // int potentiometer_2 = read_pot_2_value_room_3();
 
     if (abs(potentiometer_1 - prev_potentiometer_1)  >= 3)
     {
@@ -69,7 +69,7 @@ void update_room_3()
         }
     }
 
-    if (abs(potentiometer_2 - prev_potentiometer_2)  >= 3)
+    /* if (abs(potentiometer_2 - prev_potentiometer_2)  >= 3)
     {
         prev_potentiometer_2 = potentiometer_2;
         ColorRGB current_color;
@@ -99,7 +99,7 @@ void update_room_3()
                 set_color_of_light_HSV_room_3(current_color_HSV);
                 break;
         }
-    }
+    } */
 
     // check if button is pressed, and turn on or off light
     int button_pressed = check_button_room_3();
@@ -140,14 +140,14 @@ void switch_light_room_3()
     //if (get_status_of_light_room_3()) turn_off_light_room_3();
     //else turn_on_light_room_3();
 
-    ColorRGB color = get_color_of_light_RGB_room_3();
+    ColorRGB color;
+    get_color_of_light_RGB_room_3(color);
 
     if (color.r != 0 || color.g != 0 || color.b != 0)
     {
         pwm3_set_duty((uint8_t)0, (uint8_t)0, (uint8_t)0);
     }
-
-    pwm3_set_duty((uint8_t)255, (uint8_t)255, (uint8_t)255);
+    else pwm3_set_duty((uint8_t)255, (uint8_t)255, (uint8_t)255);
 }
 
 int get_status_of_light_room_3()
@@ -155,7 +155,8 @@ int get_status_of_light_room_3()
     // return 1 if light is on otherwise 0
     //return (PORTD & ((1 << RED_LED) | (1 << GREEN_LED) | (1 << BLUE_LED))) != 0;
 
-    ColorRGB color = get_color_of_light_RGB_room_3();
+    ColorRGB color;
+    get_color_of_light_RGB_room_3(color);
 
     if (color.r != 0 || color.g != 0 || color.b != 0)
     {
@@ -192,11 +193,11 @@ void set_color_of_light_HSV_room_3(ColorHSV hsv)
 int read_pot_1_value_room_3()
 {
     // return value of potentiometer 1
-    return adc_read(2);
-}
-
-int read_pot_2_value_room_3()
-{
-    // return value of potentiometer 2
     return adc_read(7);
 }
+
+/* int read_pot_2_value_room_3()
+{
+    // return value of potentiometer 2
+    return adc_read(2);
+} */
